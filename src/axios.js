c8412e43c9e168038f0bd9d1db3903c228d697ba
sakshi13062004 <111657188+sakshi13062004@users.noreply.github.com>
2024-08-22 15:51:53 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api/admin', // Adjust the base URL as needed
+    baseURL: 'http://localhost:8080/api/admin', 
 });
 
 export default instance;
