9b6edaa097333fbdb3b35c014d90449bfa095601
SHAIK HAFSA MUSAKN <116863515+hafsamuskan@users.noreply.github.com>
2024-08-22 16:16:18 +0530

    Delete src/services directory

--- src/services/UserService.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-const REST_API_BASE_URL = 'http://localhost:8080/api/admin/users';
-
-export const ListUsers = () => axios.get(REST_API_BASE_URL);
-
-export const createUser = (user) => axios.post(REST_API_BASE_URL);
