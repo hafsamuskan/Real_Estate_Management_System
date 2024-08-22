c811639d0ef6bd54326074a0d9dcf62af7adc83b
SHAIK HAFSA MUSAKN <116863515+hafsamuskan@users.noreply.github.com>
2024-08-22 16:01:26 +0530

    Update InterestedTenantRepository.java

--- RSE/src/main/java/com/example/rse/repository/InterestedTenantRepository.java
+++ RSE/src/main/java/com/example/rse/repository/InterestedTenantRepository.java
@@ -1,6 +1,6 @@
 package com.example.rse.repository;
 
-//InterestedTenantRepository.java
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.rse.model.InterestedTenant;
