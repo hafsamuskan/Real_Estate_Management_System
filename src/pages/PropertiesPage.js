c3264cbd9ad1aef00062a97b2b3faedb6e71bf89
sakshi13062004 <111657188+sakshi13062004@users.noreply.github.com>
2024-08-22 15:50:37 +0530

    Update PropertiesPage.js

--- src/pages/PropertiesPage.js
+++ src/pages/PropertiesPage.js
@@ -1,4 +1,3 @@
-// src/pages/PropertiesPage.js
 import React, { useState, useEffect } from 'react';
 import PropertyCard from '../components/PropertyCard';
 import '../styles/PropertiesPage.css'
@@ -7,82 +6,7 @@ import axios from 'axios';
 const PropertiesPage = () => {
 
   const [properties, setProperties] = useState([]);
-  // const properties = [
-  //   {
-  //     id: '02',
-  //     name: '02 Home',
-  //     location: 'Karnataka',
-  //     agent: 'Nikita',
-  //     date: '22-07-2022',
-  //     image: '/images/property2.jpeg',
-  //     bhkType: '2BHK',
-  //     depositPrice: '₹80,000',
-  //     ownerContact: '9876543211',
-  //     propertyStatus: 'Rented',
-  //   },
-  //   {
-  //     id: '01',
-  //     name: '01 Homes',
-  //     location: 'Kolhapur',
-  //     agent: 'Shruti Tiwari',
-  //     date: '22-07-2022',
-  //     image: '/images/property1.jpeg',
-  //     bhkType: '3BHK',
-  //     depositPrice: '₹1,00,000',
-  //     ownerContact: '9876543210',
-  //     propertyStatus: 'Available',
-  //   },
-  //   {
-  //     id: '02',
-  //     name: '02 Home',
-  //     location: 'Karnataka',
-  //     agent: 'Nikita',
-  //     date: '22-07-2022',
-  //     image: '/images/property2.jpeg',
-  //     bhkType: '2BHK',
-  //     depositPrice: '₹80,000',
-  //     ownerContact: '9876543211',
-  //     propertyStatus: 'Rented',
-  //   },
-  //   {
-  //     id: '02',
-  //     name: '02 Home',
-  //     location: 'Karnataka',
-  //     agent: 'Nikita',
-  //     date: '22-07-2022',
-  //     image: '/images/property2.jpeg',
-  //     bhkType: '2BHK',
-  //     depositPrice: '₹80,000',
-  //     ownerContact: '9876543211',
-  //     propertyStatus: 'Rented',
-  //   },
-  //   {
-  //     id: '01',
-  //     name: '01 Homes',
-  //     location: 'Kolhapur',
-  //     agent: 'Shruti Tiwari',
-  //     date: '22-07-2022',
-  //     image: '/images/property1.jpeg',
-  //     bhkType: '3BHK',
-  //     depositPrice: '₹1,00,000',
-  //     ownerContact: '9876543210',
-  //     propertyStatus: 'Available',
-  //   },
-  //   {
-  //     id: '01',
-  //     name: '01 Homes',
-  //     location: 'Kolhapur',
-  //     agent: 'Shruti Tiwari',
-  //     date: '22-07-2022',
-  //     image: '/images/property1.jpeg',
-  //     bhkType: '3BHK',
-  //     depositPrice: '₹1,00,000',
-  //     ownerContact: '9876543210',
-  //     propertyStatus: 'Available',
-  //   },
-
-  //   // Add more properties as needed
-  // ];
+ 
 
   useEffect(() => {
     // Fetch all properties
